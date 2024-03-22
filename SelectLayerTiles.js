9316110a1bd075ce2493f9e527e43ff518659cb2
eishiya <70545646+eishiya@users.noreply.github.com>
2024-03-22 09:49:48 -0400

    SelectLayerTiles: Support multiple selected layers.

--- SelectLayerTiles.js
+++ SelectLayerTiles.js
@@ -1,25 +1,29 @@
 /* 	Select Layer Tiles by eishiya, last updated 22 Mar 2024
 
 	Adds an action to the Layer context menu to select
-	all of its non-empty tiles. Only works for Tile Layers.
+	all of the selected Tile Layers' non-empty tiles.
 	
 	This opens up various workflows, such as quickly filling the shape
 	of a layer with shift+bucket fill, "saving" selections as layers,
 	and being able to quickly use a layer as a faux mask.
-	
-	It's essentially the same as Select Same Tile on Empty + Inverse Selection,
-	but as a right-click menu option (or via shortcut, if you wish).
 */
 const selectLayerTiles = tiled.registerAction("SelectLayerTiles", function(action) {	
 	let map = tiled.activeAsset;
 	if(!map || !map.isTileMap)
 		return;
 	
-	let layer = map.currentLayer;
-	if(!layer || !layer.isTileLayer)
-		return;
+	//let layer = map.currentLayer;
+	let layersSelected = 0;
+	for(let layer of map.selectedLayers) {
+		if(!layer.isTileLayer)
+			continue;
+		if(layersSelected = 0)
+			map.selectedArea.set(layer.region());
+		else
+			map.selectedArea.add(layer.region());
+	}
 	
-	map.selectedArea.set(layer.region());
+	
 });
 selectLayerTiles.text = "Select Layer Tiles";
 
@@ -33,7 +37,15 @@ selectLayerTiles.updateActive = function() {
 	if(asset.isTileMap) {
 		selectLayerTiles.previousAsset = asset;
 		asset.selectedLayersChanged.connect(selectLayerTiles.updateActive);
-		if(asset.currentLayer && asset.currentLayer.isTileLayer)
+		
+		let tileLayersFound = false;
+		for(let layer of asset.selectedLayers) {
+			if(layer.isTileLayer) {
+				tileLayersFound = true;
+				break;
+			}
+		}
+		if(tileLayersFound)
 			selectLayerTiles.enabled = true;
 		else
 			selectLayerTiles.enabled = false;
