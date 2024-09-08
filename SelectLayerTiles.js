f4a51aa1fdc57b5ac3748b5ad6b39e7ef5b6a2e5
eishiya <70545646+eishiya@users.noreply.github.com>
2024-09-08 16:44:46 -0400

    Select Layer Tiles: Avoid error when maps are deleted
    Checking that previousAsset is `null` isn't enough, because when a map owned by C++ is deleted, it may leave a dangling, non-null JS reference. Checking for an expected property like isTileMap is enough to make sure the object is actually valid.
    
    (Apparently I made this fix months ago and forgot to upload it xP)

--- SelectLayerTiles.js
+++ SelectLayerTiles.js
@@ -1,4 +1,4 @@
-/* 	Select Layer Tiles by eishiya, last updated 22 Mar 2024
+/* 	Select Layer Tiles by eishiya, last updated 8 Sep 2024
 
 	Adds an action to the Layer context menu to select
 	all of the selected Tile Layers' non-empty tiles.
@@ -29,7 +29,7 @@ selectLayerTiles.text = "Select Layer Tiles";
 //Only enable the action while the currentLayer is a TileLayer:
 selectLayerTiles.previousAsset = null;
 selectLayerTiles.updateActive = function() {
-	if(selectLayerTiles.previousAsset) {
+	if(selectLayerTiles.previousAsset && selectLayerTiles.previousAsset.isTileMap) {
 		selectLayerTiles.previousAsset.selectedLayersChanged.disconnect(selectLayerTiles.updateActive);
 	}
 	let asset = tiled.activeAsset;
